e7a0652d559b296024197ef139a3ff6a33b61ca1
John-Olav Storvold <johsto@knowit.no>
2019-03-20 22:36:27 +0100

    Go

--- go/main.go
+++ go/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+func fib(n uint) uint {
+  if n == 1 || n == 2 {
+  	return 1
+	} else if n == 3 {
+		return 2
+	}
+  return fib(n-1) + fib(n-2)
+}
+
 func Handler(_ context.Context, request *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	return &events.APIGatewayProxyResponse{Body: "hello", StatusCode: 200}, nil
+	return &events.APIGatewayProxyResponse{Body: strconv.FormatUint(uint64(fib(35)), 10), StatusCode: 200}, nil
 }
 
 func main() {
