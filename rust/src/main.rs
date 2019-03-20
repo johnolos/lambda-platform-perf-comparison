d9e03d1939e9ca29fb546594b6ef49b6a326c239
John-Olav Storvold <johsto@knowit.no>
2019-03-20 23:29:43 +0100

    As close to equal as possible

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,11 +2,7 @@ use lambda_runtime::{error::HandlerError, Context};
 use lambda_http::{lambda, IntoResponse, Request, Response};
 
 fn fibonacci(n: i32) -> u64 {
-    if n < 0 {
-        panic!("{} is negative", n);
-    }
     match n {
-        0 => panic!("zero is not valid"),
         1 | 2 => 1,
         3 => 2,
         _ => fibonacci(n - 1) + fibonacci(n - 2)
