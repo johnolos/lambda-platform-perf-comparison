b33c531bd0eb6fe8ba016ece31125b48d446588f
John-Olav Storvold <johsto@knowit.no>
2019-03-20 22:05:21 +0100

    Fib example of rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,22 @@
 use lambda_runtime::{error::HandlerError, Context};
 use lambda_http::{lambda, IntoResponse, Request, Response};
 
+fn fibonacci(n: i32) -> u64 {
+    if n < 0 {
+        panic!("{} is negative", n);
+    }
+    match n {
+        0 => panic!("zero is not valid"),
+        1 | 2 => 1,
+        3 => 2,
+        _ => fibonacci(n - 1) + fibonacci(n - 2)
+    }
+}
+
 fn main() {
     lambda!(handler);
 }
 
 fn handler(_e: Request, _c: Context) -> Result<impl IntoResponse, HandlerError> {
-    Ok(Response::new("hello"))
+    Ok(Response::new(fibonacci(200).to_string()))
 }
