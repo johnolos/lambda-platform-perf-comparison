514ac4018dc0f42d33df9416027df7b5e1ef93c7
John-Olav Storvold <johsto@knowit.no>
2019-03-21 15:16:15 +0100

    Running fib load testing

--- artillery.sh
+++ artillery.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # api.txt is a list of API urls
-for api in `cat apis.txt`
+for api in `cat fibonacci_apis.txt`
 do
     echo "testing $api"
-    artillery quick --duration 1200 --rate 10 --num 1 $api &
+    artillery quick --duration 1000 --rate 4 --num 1 $api &
 done
